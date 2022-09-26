0f2d8a2cae8ef10d47197f9cf179d5e289028fba
Harutyun K <110822129+harutyun-k@users.noreply.github.com>
2022-09-26 18:19:37 +0300

    Update cards

--- packages/@okta/vuepress-theme-prose/global-components/Cards.vue
+++ packages/@okta/vuepress-theme-prose/global-components/Cards.vue
@@ -1,11 +1,29 @@
 <template>
-    <div class="cards-own">
-        <slot></slot>
-    </div>
+  <div class="cards-own">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+  export default {
+    methods: {
+      isEqual(a, b) {
+        return a === b;
+      }
+    },
+    mounted() {
+      window.addEventListener("load", () => {
+         const cardsOwn = document.querySelectorAll(".cards-own");
+
+         cardsOwn.forEach(card => {
+            const cards = card.children;
+            const cardsLength = cards.length;
+
+            if (this.isEqual(cardsLength, 1)) {
+              card.classList.add("cards-own_type_secondary");
+            }
+         });
+      })
+    }
+  }
 </script>
